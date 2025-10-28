5d8cdbbd577691d0ed63a435e1d1322a2102e9a7
ChoirulArdhi <choirulardhi2@gmail.com>
2025-10-28 09:31:51 +0700

    installing app

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/halo', function () {
+    return 'halo Laravel';
+});
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
